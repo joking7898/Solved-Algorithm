a1059da2dad65745c9cb48db1475dcd3758cca2a
joking7898 <joking78981@gmail.com>
2021-01-26 12:11:20 +0900

    4-3 문제풀이

--- 파이썬_취업코테책/4-3.py
+++ 파이썬_취업코테책/4-3.py
@@ -1,5 +1,12 @@
-3,3
+x = str(input())
+a = ord(x[0])-96
+b = int(x[1])
+count = 0
+movexy = [[-2, -1],[-1, -2],[1, -2],[2, -1],[-2, 1],[-1,2],[1,2],[2,1]]
 
- -> 1,2  2,1  4,1  5,2  1,4  2,5  4,5  5,4
-
-    -2-1 -1-2 1 -2 2-1  -21  -12 12  21
+for i in range(8):
+    if (a + movexy[i][0] < 1) or (a + movexy[i][0] > 8) or (b + movexy[i][1] < 1) or (b + movexy[i][1] > 8):
+        continue
+    else:
+        count += 1
+print(count)
